35f02d2fb1fc3ef6ff5eecc54a9ad72713c248c8
SebastianMesa02 <93996738+SebastianMesa02@users.noreply.github.com>
2021-11-09 10:49:26 -0500

    Basics on python

---
+++ first_pract.py
@@ -0,0 +1,14 @@
+# PRIMER PRACTICA
+
+# Python detecta si es string, float, int, etc. es decir, no es necesario poner el tipo de variable
+
+string_var = "Hi teacher" # No es necesario ; 
+float_var = 123.45
+
+# Imprime los datos
+print(string_var)
+print(float_var)
+
+# La funcion type reconoce el tipo de variable es)
+print(type(string_var))
+print(type(float_var))
