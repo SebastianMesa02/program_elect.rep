508b4da7dff7aee5e03eb0d23c28a7c71a289bbc
SebastianMesa02 <93996738+SebastianMesa02@users.noreply.github.com>
2021-11-09 11:08:43 -0500

    Main change
    se agrega suma de valores y string

--- main.py
+++ main.py
@@ -12,3 +12,19 @@ print(float_var)
 # La funcion type reconoce el tipo de variable es)
 print(type(string_var))
 print(type(float_var))
+
+# Sumar dos enteros
+suma_int = (2+5)
+print(suma_int)
+
+# Sumar dos float
+num_1 = 3.5
+num_2 = 4.8
+suma_float = num_1+num_2
+print(suma_float)
+
+# Concatenar dos string
+string_1 = 'Hola'
+string_2 = ' Mundo'
+string_res = string_1 + string_2
+print(string_res)
