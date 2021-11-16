9f6a3df8597e08dfa56b26a7da9f084f3668ea69
Sebastian <sebastian.mesa02@uptc.edu.co>
2021-11-16 12:21:03 -0500

    Math Homework

---
+++ math_homework/python_projects/main.py
@@ -0,0 +1,13 @@
+# area de un circulo
+
+print("Area de una circunferencia")
+
+# A = pi * r^2
+
+from math import pi
+
+r = float(input('Digite el valor de r: '))
+
+Area = pi * r ** 2
+
+print('El valor del area es: ', Area)
