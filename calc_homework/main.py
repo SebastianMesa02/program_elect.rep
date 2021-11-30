72fe348bd928a2446fe3a559110cfe8b0633639a
Sebastian <sebastian.mesa02@uptc.edu.co>
2021-11-30 10:29:21 -0500

    Calc Homework Fix

--- calc_homework/main.py
+++ calc_homework/main.py
@@ -12,48 +12,56 @@ print("""
                 """)
 
 # Se lee la entrada
-opcion = input("Selecciona una opcion :")
+opcion = input("Selecciona una opcion: ")
 
 # Codigo para suma
 if opcion == "1":
+    print("\n Suma \n")
     print("Ingrese el primer numero: ")
-    firstNumber = int(input())
+    firstNumber = float(input())
     print("Ingrese el segundo numero: ")
-    secondNumber = int(input())
+    secondNumber = float(input())
 
     sum_numbers = firstNumber + secondNumber
     print("El resultado es: ", sum_numbers)
+
 # Codigo para resta
 elif opcion == "2":
+    print("\n Resta \n")
     print("Ingrese el primer numero: ")
-    firstNumber = int(input())
+    firstNumber = float(input())
     print("Ingrese el segundo numero: ")
-    secondNumber = int(input())
+    secondNumber = float(input())
 
     res_numbers = firstNumber - secondNumber
     print("El resultado es: ", res_numbers)
+
 # Codigo para multiplicacion
 elif opcion == "3":
+    print("\n Multiplicacion \n")
     print("Ingrese el primer numero: ")
-    firstNumber = int(input())
+    firstNumber = float(input())
     print("Ingrese el segundo numero: ")
-    secondNumber = int(input())
+    secondNumber = float(input())
 
     mul_numbers = firstNumber * secondNumber
     print("El resultado es: ", mul_numbers)
+
 # Codigo para division con validacion de entrada = 0
 elif opcion == "4":
+    print("\n Division \n")
     print("Ingrese el primer numero: ")
-    firstNumber = int(input())
+    firstNumber = float(input())
     print("Ingrese el segundo numero: ")
-    secondNumber = int(input())
+    secondNumber = float(input())
     if firstNumber == 0:
-        print("Division en 0 no realizada")
+        print("Division en 0 no realizada, intente con otro valor")
     elif secondNumber == 0:
-        print("Division en 0 no realizada")
+        print("Division en 0 no realizada, intente con otro valor")
     else:
         div_numbers = (firstNumber) / (secondNumber)
         print("El resultado es: ", div_numbers)
+
 # Caso de entrada incorrecta
 else:
     print("Opción no válida")
